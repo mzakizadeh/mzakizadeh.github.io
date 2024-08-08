4a5501f36b0259af475b2b245a579098f4d520d9
Mahdi Zakizadeh <mahdizakizadeh.me@gmail.com>
2024-08-08 23:00:56 +0330

    init: astro project

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const MY_NAME = 'Mahdi Zakizadeh';
+export const SITE_DESCRIPTION = "I'm Mahdi Zakizadeh and I work on Natural Language Processing. More details inside!";
