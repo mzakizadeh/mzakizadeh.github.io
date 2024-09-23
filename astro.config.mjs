ad58160fc006cb24c4c3a3a2aa63d9078b3ed707
Mahdi Zakizadeh <mahdizakizadeh.me@gmail.com>
2024-09-23 13:08:58 +0330

    feat: add reading time to blog posts chore: clean the diary entries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
+import { toString } from 'mdast-util-to-string';
 
+import preact from "@astrojs/preact";
+import getReadingTime from 'reading-time';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    // readingTime.text will give us minutes read as a friendly string,
+    // i.e. "3 min read"
+    data.astro.frontmatter.minutesRead = readingTime.text;
+  };
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [preact(), mdx(), sitemap()]
+  integrations: [preact(), mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
