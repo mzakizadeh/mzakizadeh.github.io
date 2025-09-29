d5f0ecde9fbf33aa12927066ac697758eff7fa4b
Mahdi Zakizadeh <mahdizakizadeh.me@gmail.com>
2025-09-29 14:52:47 +0330

    style: change the indentation space to 2 for better code readability

--- src/consts.ts
+++ src/consts.ts
@@ -4,11 +4,11 @@
 export const MY_NAME = 'Mahdi Zakizadeh';
 export const SITE_DESCRIPTION = "I'm Mahdi Zakizadeh and I work on Natural Language Processing. More details inside!";
 export const NAV_LINKS = [
-    { 'name': 'Home', 'to': '/' },
-    // { 'name': 'Blog', 'to': '/blog' },
-    // { 'name': 'Projects', 'to': '/projects' },
-    // { 'name': 'CV', 'to': '/files/zakizadeh.pdf' },
-    { 'name': 'Scholar', 'to': 'https://scholar.google.com/citations?hl=en&user=UEv0uSsAAAAJ' },
-    { 'name': 'Github', 'to': 'https://github.com/mzakizadeh' },
-    { 'name': 'Linkedin', 'to': 'https://linkedin.com/in/mahdizakizadeh' },
+  { 'name': 'Home', 'to': '/' },
+  // { 'name': 'Blog', 'to': '/blog' },
+  // { 'name': 'Projects', 'to': '/projects' },
+  // { 'name': 'CV', 'to': '/files/zakizadeh.pdf' },
+  { 'name': 'Scholar', 'to': 'https://scholar.google.com/citations?hl=en&user=UEv0uSsAAAAJ' },
+  { 'name': 'Github', 'to': 'https://github.com/mzakizadeh' },
+  { 'name': 'Linkedin', 'to': 'https://linkedin.com/in/mahdizakizadeh' },
 ]
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,39 +1,39 @@
 import { defineCollection, z } from 'astro:content';
 
 const diaryCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        date: z.coerce.date(),
-    }),
+  type: 'content',
+  schema: z.object({
+    date: z.coerce.date(),
+  }),
 });
 
 const blogCollection = defineCollection({
-    type: 'content',
-    // Type-check frontmatter using a schema
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        // Transform string to Date object
-        pubDate: z.coerce.date(),
-        updatedDate: z.coerce.date().optional(),
-        heroImage: z.string().optional(),
-    }),
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
 const featuredCollection = defineCollection({
-    type: 'data',
-    schema: z.object({
-        title: z.string(),
-        body: z.string(),
-        href: z.string(),
-        type: z.enum(['research', 'github', 'blog', 'project', 'publication']),
-        image: z.string().optional(),
-        tags: z.array(z.string()).optional(),
-    }),
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    body: z.string(),
+    href: z.string(),
+    type: z.enum(['research', 'github', 'blog', 'project', 'publication']),
+    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = {
-    'diary': diaryCollection,
-    'blog': blogCollection,
-    'featured': featuredCollection,
+  'diary': diaryCollection,
+  'blog': blogCollection,
+  'featured': featuredCollection,
 };
